03524700c35b453bc122ed1010518eef633107cd
Alvaro Tinoco <alvarotinocomarmol@gmail.com>
2021-12-01 14:03:37 +0100

    feat(changelog): add Accept header

--- pkg/changelog/changelog.go
+++ pkg/changelog/changelog.go
@@ -68,7 +68,8 @@ func fetch(version string) (*goquery.Document, error) {
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/92.0")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/94.0")
+	req.Header.Set("Accept", "*/*")
 
 	resp, err := client.Do(req)
 	if err != nil {
